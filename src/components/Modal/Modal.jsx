b8f7e6dc604e7af3b9dfaa8d5ce788eb45cf7110
MarinaPolich <marina.polich@gmail.com>
2022-11-02 19:40:45 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -9,7 +9,7 @@ export class Modal extends Component {
     return createPortal(
       <div className="Overlay">
         <div className="ModalBox">
-          <img src={this.props.url} />
+          <img src={this.props.url} alt="coctails" />
           <button onClick={this.props.onClose} type="button" className="Btn">
             Close
           </button>
